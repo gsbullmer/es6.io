ea3284d3d5db481c1b6a69823198ae9456654123
Greg Bullmer <gsbullmer@gmail.com>
2018-02-08 13:16:09 -0600

    Finish video 41

--- 12 - Code Quality with ESLint/bad-code.js
+++ 12 - Code Quality with ESLint/bad-code.js
@@ -1,10 +1,12 @@
-var weather = new Promise((resolve) => {
+/* globals ga twttr */
+
+const weather = new Promise((resolve) => {
   setTimeout(() => {
     resolve({ temp: 29, conditions: 'Sunny with Clouds' });
   }, 2000);
 });
 
-const tweets = new Promise((resolve, reject) => {
+const tweets = new Promise((resolve) => {
   setTimeout(() => {
     resolve(['I like cake', 'BBQ is good too!']);
   }, 500);
@@ -12,7 +14,7 @@ const tweets = new Promise((resolve, reject) => {
 
 Promise
   .all([weather, tweets])
-  .then(responses => {
+  .then((responses) => {
     const [weatherInfo, tweetInfo] = responses;
     console.log(weatherInfo, tweetInfo);
   });
@@ -22,9 +24,41 @@ const streetCarsPromise = fetch('http://data.ratp.fr/api/datasets/1.0/search/?q=
 
 Promise
   .all([postsPromise, streetCarsPromise])
-  .then(responses => {
-    return Promise.all(responses.map(res => res.json()));
-  })
-  .then(responses => {
+  .then(responses => Promise.all(responses.map(res => res.json())))
+  .then((responses) => {
     console.log(responses);
   });
+
+ga.track();
+twttr.trackConversion();
+
+/* eslint-disable */
+if (!Array.prototype.includes) {
+  Array.prototype.includes = function(searchElement /*, fromIndex*/ ) {
+    'use strict';
+    var O = Object(this);
+    var len = parseInt(O.length, 10) || 0;
+    if (len === 0) {
+      return false;
+    }
+    var n = parseInt(arguments[1], 10) || 0;
+    var k;
+    if (n >= 0) {
+      k = n;
+    } else {
+      k = len + n;
+      if (k < 0) {k = 0;}
+    }
+    var currentElement;
+    while (k < len) {
+      currentElement = O[k];
+      if (searchElement === currentElement ||
+        (searchElement !== searchElement && currentElement !== currentElement)) { // NaN !== NaN
+          return true;
+        }
+        k++;
+      }
+      return false;
+    };
+  }
+/* eslint-enable */
