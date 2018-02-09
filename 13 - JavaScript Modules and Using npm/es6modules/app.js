1edeb62e33000f11993241d69c6e79235f320c74
Greg Bullmer <gsbullmer@gmail.com>
2018-02-09 14:54:15 -0600

    Remove unused packages

--- 13 - JavaScript Modules and Using npm/es6modules/app.js
+++ 13 - JavaScript Modules and Using npm/es6modules/app.js
@@ -1,7 +1,3 @@
-import { uniq } from 'lodash';
-import insane from 'insane';
-import jsonp from 'jsonp';
-import api, { apiKey, url } from './src/config';
 import User, { createURL, gravatar } from './src/user';
 
 const greg = new User('Greg Bullmer', 'gsbullmer@gmail.com', 'gsbullmer.com');
