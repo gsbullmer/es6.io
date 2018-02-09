93825eefac16c275d105dd8c371f1e3d537da57a
Greg Bullmer <gsbullmer@gmail.com>
2018-02-09 14:52:33 -0600

    Finish video 46

--- 13 - JavaScript Modules and Using npm/es6modules/app.js
+++ 13 - JavaScript Modules and Using npm/es6modules/app.js
@@ -2,8 +2,9 @@ import { uniq } from 'lodash';
 import insane from 'insane';
 import jsonp from 'jsonp';
 import api, { apiKey, url } from './src/config';
+import User, { createURL, gravatar } from './src/user';
 
-const ages = [1, 1, 4, 52, 12, 4];
-
-console.log(apiKey, url, api);
-console.log(uniq(ages));
+const greg = new User('Greg Bullmer', 'gsbullmer@gmail.com', 'gsbullmer.com');
+const profile = createURL(greg.name);
+const avatar = gravatar(greg.email);
+console.log(profile, avatar);
